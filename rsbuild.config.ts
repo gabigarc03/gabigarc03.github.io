f46c9c0a49019dc7e687fa41acc3b484007bc972
Gabi Garcia <gabigarcia@gabis-mbp.lan>
2025-10-27 14:49:35 +0000

    Trying zephyr

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginPreact } from '@rsbuild/plugin-preact';
 import { pluginSass } from '@rsbuild/plugin-sass';
+import { withZephyr } from 'zephyr-rsbuild-plugin';
 
 export default defineConfig({
   plugins: [
@@ -8,6 +9,7 @@ export default defineConfig({
     pluginSass({
       rewriteUrls: false,
     }),
+    withZephyr(),
   ],
   html: {
     title: 'G.C. Garcia',
