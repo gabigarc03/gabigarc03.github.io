73e33c49846bc2cc2eb23b79f81eebf1de3ca330
Gabi Garcia <gabigarcia@gabis-mbp.lan>
2025-10-27 14:57:14 +0000

    Revert "Temporary fix for portfolio to work"
    This reverts commit e750995aade1699736bab8e7402e3233a9aeb04e.

--- src/App.tsx
+++ src/App.tsx
@@ -10,11 +10,6 @@ import Resume from './components/view/views/Resume';
 import Contact from './components/view/views/Contact';
 import PortfolioPage from './components/view/views/portfolio/PortfolioPage';
 import NotFound from './components/view/views/NotFound';
-import Scav from './components/view/views/portfolio/dev/Scav';
-import Starlink from './components/view/views/portfolio/design/Starlink';
-import Brightline from './components/view/views/portfolio/design/Brightline';
-import Awards from './components/view/views/portfolio/design/Awards';
-import Coupon from './components/view/views/portfolio/design/Coupon';
 
 const App = () => {
   return (
@@ -30,11 +25,6 @@ const App = () => {
             <Route path='/writing' component={Writing} />
             <Route path='/resume' component={Resume} />
             <Route path='/contact' component={Contact} />
-            <Route path='/tech-dev-scav' component={Scav} />
-            <Route path='/tech-design-starlink' component={Starlink} />
-            <Route path='/tech-design-brightline' component={Brightline} />
-            <Route path='/tech-design-awards' component={Awards} />
-            <Route path='/tech-design-coupon' component={Coupon} />
             <Route default component={NotFound} />
           </Router>
           <Footer />
