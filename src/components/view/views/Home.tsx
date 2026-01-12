67ae0584aa0a4e7410894e018516e462b2c6805d
Gabi Garcia <17114209+gabigarc03@users.noreply.github.com>
2026-01-12 12:40:00 +0000

    other small update

--- src/components/view/views/Home.tsx
+++ src/components/view/views/Home.tsx
@@ -2,11 +2,17 @@ import { Component } from 'preact';
 import View from '../View';
 
 export default class Home extends Component {
-  render () {
+  render() {
     return (
       <View name='home'>
-        <img alt='Gabriela Garcia' src={require('../../../assets/images/headshot.jpg')}></img>
-        <p>I am a writer and software engineer currently seeking work in London, England.</p>
+        <img
+          alt='Gabriela Garcia'
+          src={require('../../../assets/images/headshot.jpg')}
+        ></img>
+        <p>
+          I am a writer, software engineer, and UX designer seeking work in
+          London, England.
+        </p>
       </View>
     );
   }
